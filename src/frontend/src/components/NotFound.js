c8539677141bd0e51865332dc16982a3c445a4e3
Dragan <90929387+energydrinksjunkie@users.noreply.github.com>
2024-07-27 09:00:42 +0200

    added css support for mobile devices

--- src/frontend/src/components/NotFound.js
+++ src/frontend/src/components/NotFound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
     <>
         <div className="not-found-container">
         <img src={notFoundImage} alt="Page Not Found" className="not-found-image" />
-        <h2>Page Not Found</h2>
+        <p className='not-found-title'>Page Not Found</p>
         <p>The page you're looking for doesn't exist.</p>
         </div>
     </>
