b2cd32a3f449211e2d7f4a30b654ef5121a3f0c7
Dragan <90929387+energydrinksjunkie@users.noreply.github.com>
2024-07-27 08:10:41 +0200

    rewrote home page css

--- src/frontend/src/components/Home.js
+++ src/frontend/src/components/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
   return (
     <>
         <div className="home-container">
-        <h1>Welcome to Handshake Cracker</h1>
-        <p>
-            Handshake Cracker is a powerful tool designed to help you analyze and crack WPA handshakes. With our user-friendly interface and advanced capabilities, you can efficiently manage and break through encrypted networks.
+        <p className='title-home'>Welcome to WireMess</p>
+        <p className='desc'>
+            WireMess is a powerful tool designed to help you analyze and crack WPA handshakes. With its user-friendly interface and advanced capabilities, you can efficiently manage and break through encrypted networks.
         </p>
         <Link to="/login" className="login-link">
             Get Started
